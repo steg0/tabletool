14915d7c0276f883ba7281f4e15174328a3833a8
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-10-10 10:09:21 +0000

    remove unneeded imports
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4233 7869264c-0633-0410-bb99-d298f671b333

--- src/main/java/de/steg0/deskapps/tabletool/ResultSetTableModel.java
+++ src/main/java/de/steg0/deskapps/tabletool/ResultSetTableModel.java
@@ -6,8 +6,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.event.EventListenerList;
-import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
