1a8fd78bf0332082dd72d4164e4cb9e47aad5a92
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-10-27 11:38:07 +0000

    JUnit setup,test for Csv parser(simple)
    this required pulling a newer Surefire plugin.
    I expect this to be temporary.
    
    
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4301 7869264c-0633-0410-bb99-d298f671b333

---
+++ src/test/java/de/steg0/deskapps/tabletool/GrowingCsvBufferTest.java
@@ -0,0 +1,34 @@
+package de.steg0.deskapps.tabletool;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GrowingCsvBufferTest
+{
+
+    @Test
+    void multilinefield()
+    {
+        String input = "a,\"b\nc\",d\n";
+        var g = new GrowingCsvBuffer();
+        g.append(input);
+        String[] header = g.getHeader();
+        assertEquals("a",header[0]);
+        assertEquals("b\nc",header[1]);
+        assertEquals("d",header[2]);
+    }
+
+    @Test
+    void quoteinfield()
+    {
+        String input = "a,\"b\"\"c\",d\n";
+        var g = new GrowingCsvBuffer();
+        g.append(input);
+        String[] header = g.getHeader();
+        assertEquals("a",header[0]);
+        assertEquals("b\"c",header[1]);
+        assertEquals("d",header[2]);
+    }
+
+}
