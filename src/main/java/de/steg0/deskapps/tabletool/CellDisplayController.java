53cfb175fb85befa6ac8a22a576ff8ff2dd3033d
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-10-28 00:06:06 +0000

    add prefix display for BLOBS, as hexdump
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4320 7869264c-0633-0410-bb99-d298f671b333

--- src/main/java/de/steg0/deskapps/tabletool/CellDisplayController.java
+++ src/main/java/de/steg0/deskapps/tabletool/CellDisplayController.java
@@ -1,12 +1,15 @@
 package de.steg0.deskapps.tabletool;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.LineNumberReader;
+import java.sql.Blob;
 import java.sql.Clob;
 import java.sql.SQLException;
 import java.util.Date;
@@ -63,14 +66,10 @@ class CellDisplayController
     void show(Object value)
     throws SQLException,IOException
     {
-        var dialog = new JDialog(parent,"Cell display",true);
-
-        dialog.setLocationRelativeTo(parent.getContentPane());
-        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        dialog.getContentPane().setLayout(new BorderLayout());
-        
-        var textarea = new JTextArea(8,60);
+        var textarea = new JTextArea(10,72);
         textarea.setEditable(false);
+        
+        String dialogtitle;
         
         if(value instanceof Clob)
         {
@@ -81,11 +80,53 @@ class CellDisplayController
                 b.append(line);
             }
             textarea.setText(b.toString());
+            dialogtitle = "CLOB display";
+        }
+        else if(value instanceof Blob)
+        {
+            var blob = (Blob)value;
+            try(InputStream is = blob.getBinaryStream())
+            {
+                var hex=new StringBuilder();
+                var plain=new StringBuilder();
+                int i=0;
+                for(;i<16*0x100;i++)
+                {
+                    int b=is.read();
+                    if(b==-1)
+                    {
+                        for(int j=0;j<16-i%16;j++) hex.append("   ");
+                        hex.append(plain);
+                        break;
+                    }
+                    if(i%16==0)
+                    {
+                        if(i>0) hex.append(plain).append('\n');
+                        plain.setLength(0);
+                        hex.append(String.format("%04x ",i));
+                    }
+                    hex.append(String.format("%02x ",b));
+                    plain.append(b>=0&&b<32?'.':(char)b);
+                }
+                textarea.setFont(new Font(
+                        Font.MONOSPACED,
+                        Font.PLAIN,
+                        textarea.getFont().getSize()));
+                textarea.setText(hex.toString());
+                dialogtitle = "BLOB bytes 0 to "+i+" of "+blob.length();
+            }
         }
         else
         {
-            textarea.setText(value.toString());
+            if(value!=null) textarea.setText(value.toString());
+            dialogtitle = "Scalar value display";
         }
+        
+        var dialog = new JDialog(parent,dialogtitle,true);
+
+        dialog.setLocationRelativeTo(parent.getContentPane());
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.getContentPane().setLayout(new BorderLayout());
         
         textarea.addKeyListener(new KeyListener()
         {
