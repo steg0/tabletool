6c1893aadf4d94be19393d86d62dbdb23399721c
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-10-31 12:08:44 +0000

    use StandardCharsets instead of Charset
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4399 7869264c-0633-0410-bb99-d298f671b333

--- src/main/java/de/steg0/deskapps/tabletool/HelpAction.java
+++ src/main/java/de/steg0/deskapps/tabletool/HelpAction.java
@@ -4,7 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import javax.swing.AbstractAction;
 import javax.swing.JDialog;
@@ -32,7 +32,7 @@ class HelpAction extends AbstractAction
     {
         try(var r=new BufferedReader(new InputStreamReader(
                 getClass().getResourceAsStream("/README.txt"),
-                Charset.forName("UTF-8"))))
+                StandardCharsets.UTF_8)))
         {
             var dialog = new JDialog(parent,"Myriad README",false);
             dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
