ee21ce190846e4cec71c38e286e2388f7a31d13d
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-10-31 09:45:20 +0000

    create ... also CallableStatement
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4394 7869264c-0633-0410-bb99-d298f671b333

--- src/main/java/de/steg0/deskapps/tabletool/ConnectionWorker.java
+++ src/main/java/de/steg0/deskapps/tabletool/ConnectionWorker.java
@@ -91,6 +91,7 @@ class ConnectionWorker
         {
             if(text.startsWith("begin") || 
                text.startsWith("declare") ||
+               text.startsWith("create") ||
                text.startsWith("{")
             )
             {
