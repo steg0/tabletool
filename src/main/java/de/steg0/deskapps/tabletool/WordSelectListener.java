792dbcd2e5460e9b796849e6cb9d69b5276032b1
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-11-28 14:18:58 +0000

    add select-line mode
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4548 7869264c-0633-0410-bb99-d298f671b333

--- src/main/java/de/steg0/deskapps/tabletool/WordSelectListener.java
+++ src/main/java/de/steg0/deskapps/tabletool/WordSelectListener.java
@@ -38,36 +38,36 @@ extends MouseAdapter
     @Override
     public void mousePressed(MouseEvent e)
     {
-        clickCount = e.getClickCount(); 
-        if(clickCount == 2)
+        clickCount = e.getClickCount();
+        switch(clickCount)
         {
+        case 2:
             clickPos = textarea.viewToModel2D(e.getPoint());
-            logger.log(Level.FINE,"clickPos={0}",clickPos);
+            logger.log(Level.FINE,"selectWord,clickPos={0}",clickPos);
             selectWord();
+            break;
+        case 3:
+            clickPos = textarea.viewToModel2D(e.getPoint());
+            logger.log(Level.FINE,"selectLine,clickPos={0}",clickPos);
+            selectLine();
         }
     }
 
     @Override
     public void mouseDragged(MouseEvent e)
     {
-        if(clickCount == 2)
+        switch(clickCount)
         {
+        case 2:
             dragPos = textarea.viewToModel2D(e.getPoint());
-            logger.log(Level.FINE,"dragPos={0}",dragPos);
-            updateSelection();
+            logger.log(Level.FINE,"updateWordSelection,dragPos={0}",dragPos);
+            updateWordSelection();
+            break;
+        case 3:
+            dragPos = textarea.viewToModel2D(e.getPoint());
+            logger.log(Level.FINE,"updateLineSelection,dragPos={0}",dragPos);
+            updateLineSelection();
         }
-    }
-
-    int getWordEndPos(String text,int initPos)
-    {
-        int i = initPos;
-        while(i < text.length() && (
-              Character.isJavaIdentifierStart(text.charAt(i)) ||
-              Character.isJavaIdentifierPart(text.charAt(i))))
-        {
-            i++;
-        }
-        return i;
     }
 
     int getWordStartPos(String text,int initPos)
@@ -82,6 +82,18 @@ extends MouseAdapter
         return i + 1;
     }
 
+    int getWordEndPos(String text,int initPos)
+    {
+        int i = initPos;
+        while(i < text.length() && (
+              Character.isJavaIdentifierStart(text.charAt(i)) ||
+              Character.isJavaIdentifierPart(text.charAt(i))))
+        {
+            i++;
+        }
+        return i;
+    }
+
     void selectWord()
     {
         if(clickPos<0) return;
@@ -94,7 +106,7 @@ extends MouseAdapter
         textarea.select(newSelection[0],newSelection[1]);
     }
     
-    void updateSelection()
+    void updateWordSelection()
     {
         if(dragPos<0) return;
         String text = textarea.getText();
@@ -106,6 +118,55 @@ extends MouseAdapter
         Integer[] newSelection = {
                 getWordStartPos(text,selection[0]),
                 getWordEndPos(text,selection[1])
+        };
+        logger.log(Level.FINE,"newstart={0},newend={1}",newSelection);
+        textarea.select(newSelection[0],newSelection[1]);
+    }
+
+    int getLineStartPos(String text,int initPos)
+    {
+        int i = initPos;
+        while(i >= 0 && text.charAt(i) != '\n')
+        {
+            i--;
+        }
+        return i + 1;
+    }
+
+    int getLineEndPos(String text,int initPos)
+    {
+        int i = initPos;
+        while(i < text.length() && text.charAt(i) != '\n')
+        {
+            i++;
+        }
+        return i;
+    }
+
+    void selectLine()
+    {
+        if(clickPos<0) return;
+        String text = textarea.getText();
+        Integer[] newSelection = {
+                getLineStartPos(text,clickPos),
+                getLineEndPos(text,clickPos)
+        };
+        logger.log(Level.FINE,"newstart={0},newend={1}",newSelection);
+        textarea.select(newSelection[0],newSelection[1]);
+    }
+    
+    void updateLineSelection()
+    {
+        if(dragPos<0) return;
+        String text = textarea.getText();
+        Integer[] selection = {
+                max(min(clickPos,dragPos),textarea.getSelectionStart()),
+                max(clickPos,textarea.getSelectionEnd())
+        };
+        logger.log(Level.FINE,"start={0},end={1}",selection);
+        Integer[] newSelection = {
+                getLineStartPos(text,selection[0]),
+                getLineEndPos(text,selection[1])
         };
         logger.log(Level.FINE,"newstart={0},newend={1}",newSelection);
         textarea.select(newSelection[0],newSelection[1]);
