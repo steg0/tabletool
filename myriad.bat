fc74f31424f0107c68d3e336150903b7207d611f
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-11-28 11:26:01 +0000

    Add basic logging infrastructure
    The main idea behind logging is to provide a way
    to debug concurrency issues, mainly. Going to use
    it for some other stuff too but not as critical.
    
    
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4543 7869264c-0633-0410-bb99-d298f671b333

--- myriad.bat
+++ myriad.bat
@@ -1,5 +1,6 @@
-%JAVA_HOME%\bin\java -XX:+UseSerialGC ^
+%JAVA_HOME%\bin\java %JAVA_OPTS% -XX:+UseSerialGC ^
     -Dfile.encoding=UTF-8 ^
+    -Djava.util.logging.config.file=logging.properties ^
     -p %USERPROFILE%\.m2\repository\com\oracle\ojdbc7\12.1.0.1\ojdbc7-12.1.0.1.jar;target\classes ^
     -m de.steg0.deskapps.tabletool/de.steg0.deskapps.tabletool.Myriad ^
     -config test.myriad.properties ^
