9527ba446f59d89afec6d9f2f9217f0bfaa97ce2
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-11-29 14:12:13 +0000

    new sample script using javaw and not preselecting a workspace
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4569 7869264c-0633-0410-bb99-d298f671b333

---
+++ myriad.bat
@@ -0,0 +1,8 @@
+@set MODULEPATH=%USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.1.4.4\jcc-11.1.4.4.jar;p:\jdbc_sqlj\db2jcc4.jar
+@start %JAVA_HOME%\bin\javaw -XX:+UseSerialGC ^
+    -Dfile.encoding=UTF-8 ^
+    -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel ^
+    -jar target/myriad.jar ^
+    -config test.myriad.properties ^
+    -- ^
+    %1 %2 %3 %4 %5 %6 %7 %8 %9
