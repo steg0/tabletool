9628f9628a9abead2ef30ace1342b71b30a44dae
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-11-29 10:17:08 +0000

    adjust for nt2(which also has newer odjc,+db2)
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4560 7869264c-0633-0410-bb99-d298f671b333

--- test.myriad.bat
+++ test.myriad.bat
@@ -1,7 +1,7 @@
 %JAVA_HOME%\bin\java %JAVA_OPTS% -XX:+UseSerialGC ^
     -Dfile.encoding=UTF-8 ^
     -Djava.util.logging.config.file=logging.properties ^
-    -p %USERPROFILE%\.m2\repository\com\oracle\ojdbc7\12.1.0.1\ojdbc7-12.1.0.1.jar;%USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;p:\jdbc_sqlj\db2jcc4.jar;target\classes ^
+    -p %USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.1.4.4\jcc-11.1.4.4.jar;p:\jdbc_sqlj\db2jcc4.jar;target\classes ^
     -m de.steg0.deskapps.tabletool/de.steg0.deskapps.tabletool.Myriad ^
     -config test.myriad.properties ^
     test.myr.xml
