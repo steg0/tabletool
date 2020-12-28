047c29300b22ee63fa9c088b4ee887bc6fb6f7d9
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2020-12-28 21:51:19 +0000

    streamline path arguments
    also, MODULEPATH is a myth
    
    
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4614 7869264c-0633-0410-bb99-d298f671b333

--- myriadw.bat
+++ myriadw.bat
@@ -1,7 +1,7 @@
-@set MODULEPATH=%USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.1.4.4\jcc-11.1.4.4.jar;p:\jdbc_sqlj\db2jcc4.jar
 @start %JAVA_HOME%\bin\javaw %JAVA_OPTS% -XX:+UseSerialGC ^
     -Dfile.encoding=UTF-8 ^
     -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel ^
+    -p %USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.1.4.4\jcc-11.1.4.4.jar ^
     -jar target/myriad.jar ^
     -config test.myriad.properties ^
     -- ^
