e7686ca64acc7a8b796bc0df21c3df39f8c3f973
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2021-02-21 20:53:26 +0000

    add example for runtime logging config
    this logging could be enabled with JMX, by setting the
    "tabletool.editor" level to FINE.
    
    
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4661 7869264c-0633-0410-bb99-d298f671b333

--- tabtypew.bat
+++ tabtypew.bat
@@ -1,5 +1,6 @@
 @start %JAVA_HOME%\bin\javaw %JAVA_OPTS% -XX:+UseSerialGC ^
     -Dfile.encoding=UTF-8 ^
+    -Djava.util.logging.config.file=runtime.logging.properties ^
     -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel ^
     -p %USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.1.4.4\jcc-11.1.4.4.jar ^
     -jar target/tabtype.jar ^
--- test.tabtype.bat
+++ test.tabtype.bat
@@ -1,6 +1,6 @@
 %JAVA_HOME%\bin\java %JAVA_OPTS% -ea -XX:+UseSerialGC ^
     -Dfile.encoding=UTF-8 ^
-    -Djava.util.logging.config.file=logging.properties ^
+    -Djava.util.logging.config.file=test.logging.properties ^
     -p %USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.1.4.4\jcc-11.1.4.4.jar;target\classes ^
     -m de.steg0.deskapps.tabletool/de.steg0.deskapps.tabletool.Tabtype ^
     -config test.tabtype.properties ^
