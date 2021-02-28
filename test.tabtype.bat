a5db640061308f87cb125054abe159ae74a8f64f
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2021-02-28 20:59:33 +0000

    debug setting can really be the default
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4669 7869264c-0633-0410-bb99-d298f671b333

--- test.tabtype.bat
+++ test.tabtype.bat
@@ -1,4 +1,5 @@
 %JAVA_HOME%\bin\java %JAVA_OPTS% -ea -XX:+UseSerialGC ^
+    -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9001 ^
     -Dfile.encoding=UTF-8 ^
     -Djava.util.logging.config.file=test.logging.properties ^
     -p %USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.1.4.4\jcc-11.1.4.4.jar;target\classes ^
