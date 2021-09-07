4b320d26566cab3616d7d2b8d8bc506f1bc696e9
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2021-09-07 08:41:45 +0000

    remove compilation warning
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4780 7869264c-0633-0410-bb99-d298f671b333

--- src/main/java/de/steg0/deskapps/tabletool/JdbcBufferController.java
+++ src/main/java/de/steg0/deskapps/tabletool/JdbcBufferController.java
@@ -12,8 +12,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
