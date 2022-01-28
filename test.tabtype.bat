6ab13b2acba729051cdcd8dd0cebd36e151eba4b
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2022-01-28 23:26:43 +0000

    script more of the testing setup
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@4979 7869264c-0633-0410-bb99-d298f671b333

--- test.tabtype.bat
+++ test.tabtype.bat
@@ -2,7 +2,7 @@
     -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9001 ^
     -Dfile.encoding=UTF-8 ^
     -Djava.util.logging.config.file=test.logging.properties ^
-    -p %USERPROFILE%\.m2\repository\org\apache\derby\derbyshared\10.15.2.0\derbyshared-10.15.2.0.jar;%USERPROFILE%\.m2\repository\org\apache\derby\derby\10.15.2.0\derby-10.15.2.0.jar;%USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.1.4.4\jcc-11.1.4.4.jar;target\classes ^
+    -p %USERPROFILE%\.m2\repository\org\apache\derby\derbyshared\10.15.2.0\derbyshared-10.15.2.0.jar;%USERPROFILE%\.m2\repository\org\apache\derby\derby\10.15.2.0\derby-10.15.2.0.jar;%USERPROFILE%\.m2\repository\com\oracle\ojdbc10\19.3\ojdbc10-19.3.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.5.7.0\jcc-11.5.7.0.jar;target\classes ^
     -m de.steg0.deskapps.tabletool/de.steg0.deskapps.tabletool.Tabtype ^
     -config test.tabtype.properties ^
     test.tt.xml
