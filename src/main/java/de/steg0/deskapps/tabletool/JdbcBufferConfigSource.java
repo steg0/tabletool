96a15de9935eca608abdfa8e540cc8a456fa42a5
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2022-10-10 20:13:46 +0000

    add forgotten file
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@5427 7869264c-0633-0410-bb99-d298f671b333

---
+++ src/main/java/de/steg0/deskapps/tabletool/JdbcBufferConfigSource.java
@@ -0,0 +1,22 @@
+package de.steg0.deskapps.tabletool;
+
+class JdbcBufferConfigSource
+{
+    PropertyHolder propertyHolder;
+    ConnectionListModel connectionListModel;
+
+    JdbcBufferConfigSource(PropertyHolder propertyHolder,
+            ConnectionListModel connectionListModel)
+    {
+        this.propertyHolder = propertyHolder;
+        this.connectionListModel = connectionListModel;
+    }
+
+    String getCompletionTemplate()
+    {
+        Connections.ConnectionState connectionState = 
+            (Connections.ConnectionState)connectionListModel.getSelectedItem();
+        return connectionState.info().completionTemplate;
+    }
+    
+}
