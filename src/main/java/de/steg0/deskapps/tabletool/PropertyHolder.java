b36b1c0ec4d5c4936287c94490965bb54e808dbb
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2022-10-26 19:35:50 +0000

    remove unneeded import
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@5459 7869264c-0633-0410-bb99-d298f671b333

--- src/main/java/de/steg0/deskapps/tabletool/PropertyHolder.java
+++ src/main/java/de/steg0/deskapps/tabletool/PropertyHolder.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class PropertyHolder
