a945d21d2e8b58e8b8d30437fcc965c197858b30
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2022-11-19 09:12:52 +0000

    update Oracle drivers/add PKI
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@5546 7869264c-0633-0410-bb99-d298f671b333

--- test.tabtype.bat
+++ test.tabtype.bat
@@ -3,7 +3,18 @@
     -Dfile.encoding=UTF-8 ^
     -Dswing.plaf.metal.controlFont="Segoe UI" ^
     -Djava.util.logging.config.file=test.logging.properties ^
-    -p %USERPROFILE%\.m2\repository\org\apache\derby\derbyshared\10.15.2.0\derbyshared-10.15.2.0.jar;%USERPROFILE%\.m2\repository\org\apache\derby\derby\10.15.2.0\derby-10.15.2.0.jar;%USERPROFILE%\.m2\repository\org\postgresql\postgresql\42.3.1\postgresql-42.3.1.jar;%USERPROFILE%\.m2\repository\com\oracle\ojdbc\ojdbc10\19.3.0.0\ojdbc10-19.3.0.0.jar;%USERPROFILE%\.m2\repository\com\oracle\database\xml\xdb\19.3.0.0\xdb-19.3.0.0.jar;%USERPROFILE%\.m2\repository\com\oracle\database\xml\xmlparserv2\19.3.0.0\xmlparserv2-19.3.0.0.jar;%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.5.7.0\jcc-11.5.7.0.jar;target\classes ^
+    -p ^
+%USERPROFILE%\.m2\repository\org\apache\derby\derbyshared\10.15.2.0\derbyshared-10.15.2.0.jar;^
+%USERPROFILE%\.m2\repository\org\apache\derby\derby\10.15.2.0\derby-10.15.2.0.jar;^
+%USERPROFILE%\.m2\repository\org\postgresql\postgresql\42.3.1\postgresql-42.3.1.jar;^
+%USERPROFILE%\.m2\repository\com\oracle\database\jdbc\ojdbc11\21.7.0.0\ojdbc10-21.7.0.0.jar;^
+%USERPROFILE%\.m2\repository\com\oracle\database\xml\xdb\21.7.0.0\xdb-21.7.0.0.jar;^
+%USERPROFILE%\.m2\repository\com\oracle\database\xml\xmlparserv2\21.7.0.0\xmlparserv2-21.7.0.0.jar;^
+%USERPROFILE%\.m2\repository\com\oracle\database\security\osdt_core\21.7.0.0\osdt_core-21.7.0.0.jar;^
+%USERPROFILE%\.m2\repository\com\oracle\database\security\osdt_cert\21.7.0.0\osdt_cert-21.7.0.0.jar;^
+%USERPROFILE%\.m2\repository\com\oracle\database\security\oraclepki\21.7.0.0\oraclepki-21.7.0.0.jar;^
+%USERPROFILE%\.m2\repository\com\ibm\db2\jcc\11.5.7.0\jcc-11.5.7.0.jar;^
+target\classes ^
     -m de.steg0.deskapps.tabletool/de.steg0.deskapps.tabletool.Tabtype ^
     -config test.tabtype.properties ^
     test.tt.xml
