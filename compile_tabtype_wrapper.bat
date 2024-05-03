5be85c015a70804ead22ed911cca7ddcb7c20b28
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2024-05-03 10:03:26 +0000

    add icon for launcher
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@6558 7869264c-0633-0410-bb99-d298f671b333

--- compile_tabtype_wrapper.bat
+++ compile_tabtype_wrapper.bat
@@ -1,1 +1,1 @@
-%WINDIR%\microsoft.net\framework\v4.0.30319\csc.exe /target:winexe tabtype.cs
+%WINDIR%\microsoft.net\framework\v4.0.30319\csc.exe /win32icon:src\main\resources\de\steg0\deskapps\tabletool\icon32.ico /target:winexe tabtype.cs
