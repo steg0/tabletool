9dde92a6b1cc857a725d03af7e1a23501c098378
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2024-05-03 09:53:12 +0000

    put more stuff in %appdata%; ws changes
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@6557 7869264c-0633-0410-bb99-d298f671b333

--- tabtype.cs
+++ tabtype.cs
@@ -1,44 +1,51 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace Tabtype
 {
+    /* A launcher for Windows. */
     public class Tabtype
     {
         static void Main(string[] args)
         {
-            string procbin=Environment.ExpandEnvironmentVariables(
-                "%USERPROFILE%\\proj\\jdk-21.0.1\\bin\\javaw.exe");
-            string procargs=Environment.ExpandEnvironmentVariables("-XX:+UseSerialGC " +
-                "-Dfile.encoding=UTF-8 " +
-                "-Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel " +
-                "-Djava.util.logging.config.file=%USERPROFILE%\\proj\\tabtype\\runtime.logging.properties " +
-                "-p " +
-                "%USERPROFILE%\\.m2\\repository\\org\\apache\\derby\\derbyshared\\10.15.2.0\\derbyshared-10.15.2.0.jar;" +
-                "%USERPROFILE%\\.m2\\repository\\org\\apache\\derby\\derby\\10.15.2.0\\derby-10.15.2.0.jar;" +
-                "%USERPROFILE%\\.m2\\repository\\org\\postgresql\\postgresql\\42.3.1\\postgresql-42.3.1.jar;" +
-                "%USERPROFILE%\\.m2\\repository\\com\\oracle\\database\\jdbc\\ojdbc11\\21.7.0.0\\ojdbc11-21.7.0.0.jar;" +
-                "%USERPROFILE%\\.m2\\repository\\com\\oracle\\database\\xml\\xdb\\21.7.0.0\\xdb-21.7.0.0.jar;" +
-                "%USERPROFILE%S\\.m2\\repository\\com\\ibm\\db2\\jcc\\11.5.7.0\\jcc-11.5.7.0.jar " +
-                "-jar %USERPROFILE%\\proj\\tabtype\\tabtype.jar " +
-                "-config %APPDATA%\\tabtype\\tabtype.properties " +
-                "-config %USERPROFILE%\\Documents\\src\\tabtype\\test.tabtype.xml " +
-                "-- ");
-
-            foreach(string arg in args)
+            string javaConfigPath = Environment.ExpandEnvironmentVariables(
+                    "%APPDATA%\\tabtype\\javaw.conf");
+            using(StreamReader sr = new StreamReader(javaConfigPath))
             {
-                procargs += " \"" + arg.Replace("\"","\"\"") + "\"";
+                string procbin = sr.ReadLine();
+                string procargs=Environment.ExpandEnvironmentVariables(
+                        "-XX:+UseSerialGC " +
+                        "-Dfile.encoding=UTF-8 " +
+                        "-Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel " +
+                        "-Djava.util.logging.config.file=%APPDATA%\\tabtype\\logging.properties " +
+                        "-p " +
+                        "%USERPROFILE%\\.m2\\repository\\org\\apache\\derby\\derbyshared\\10.15.2.0\\derbyshared-10.15.2.0.jar;" +
+                        "%USERPROFILE%\\.m2\\repository\\org\\apache\\derby\\derby\\10.15.2.0\\derby-10.15.2.0.jar;" +
+                        "%USERPROFILE%\\.m2\\repository\\org\\postgresql\\postgresql\\42.3.1\\postgresql-42.3.1.jar;" +
+                        "%USERPROFILE%\\.m2\\repository\\com\\oracle\\database\\jdbc\\ojdbc11\\21.7.0.0\\ojdbc11-21.7.0.0.jar;" +
+                        "%USERPROFILE%\\.m2\\repository\\com\\oracle\\database\\xml\\xdb\\21.7.0.0\\xdb-21.7.0.0.jar;" +
+                        "%USERPROFILE%S\\.m2\\repository\\com\\ibm\\db2\\jcc\\11.5.7.0\\jcc-11.5.7.0.jar " +
+                        "-jar %APPDATA%\\tabtype\\tabtype.jar " +
+                        "-config %APPDATA%\\tabtype\\tabtype.properties.xml " +
+                        "-config %APPDATA%\\tabtype\\tabtype.properties " +
+                        "-- ");
+
+                foreach(string arg in args)
+                {
+                    procargs += " \"" + arg.Replace("\"","\"\"") + "\"";
+                }
+
+                ProcessStartInfo pStartInfo = new ProcessStartInfo(procbin,
+                        procargs);
+                pStartInfo.UseShellExecute = false;
+                pStartInfo.CreateNoWindow = true;
+
+                Process proc = new Process();
+                proc.StartInfo = pStartInfo;
+
+                proc.Start();
             }
-
-            ProcessStartInfo pStartInfo = new ProcessStartInfo(
-                procbin,procargs);
-            pStartInfo.UseShellExecute = false;
-            pStartInfo.CreateNoWindow = true;
-
-            Process proc = new Process();
-            proc.StartInfo = pStartInfo;
-
-            proc.Start();
         }
     }
 }
