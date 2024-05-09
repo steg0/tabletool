d23e5b7695a631c163240eb09fbe82405947aabf
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2024-05-09 19:12:01 +0000

    bug 278 -- try Windows LAF again
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@6584 7869264c-0633-0410-bb99-d298f671b333

--- tabtype.cs
+++ tabtype.cs
@@ -27,8 +27,8 @@ namespace Tabtype
             string procargs=Environment.ExpandEnvironmentVariables(
                     "-XX:+UseSerialGC " +
                     "-Dfile.encoding=UTF-8 " +
-                    "-Dswing.plaf.metal.controlFont=\"Segoe UI\" " +
-                    //"-Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel " +
+                    // "-Dswing.plaf.metal.controlFont=\"Segoe UI\" " +
+                    "-Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel " +
                     "-p " +
                     "\"" + String.Join(";",deps).Replace("\"","\"\"") + "\" " +
                     "-jar " + appDirectory.Replace("\"","\"\"") + "tabtype.jar " +
