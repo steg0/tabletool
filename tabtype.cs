c0647905ffd82f933e92acbba6f2631cbf3bab5a
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2024-12-18 11:05:37 +0000

    Revert "remove debugger to support more than one instance"
    This reverts commit 1c883ff1b8b6d8164df9f1335869fe5178c80c01.
    
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@7065 7869264c-0633-0410-bb99-d298f671b333

--- tabtype.cs
+++ tabtype.cs
@@ -26,6 +26,7 @@ namespace Tabtype
             string[] deps = Directory.GetFiles(appDirectory + "dependency");
             string procargs=Environment.ExpandEnvironmentVariables(
                     "-XX:+UseSerialGC " +
+                    "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9011 " +
                     "-Dfile.encoding=UTF-8 " +
                     "-Dswing.plaf.metal.controlFont=\"Segoe UI\" " +
                     //"-Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel " +
