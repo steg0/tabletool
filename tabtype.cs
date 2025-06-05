a2ea99bf0ee891808e0ff44c81bb26ca964b0f9f
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2025-06-05 17:06:11 +0000

    revert JAVA_OPTS test
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@7260 7869264c-0633-0410-bb99-d298f671b333

--- tabtype.cs
+++ tabtype.cs
@@ -25,7 +25,6 @@ namespace Tabtype
             string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
             string[] deps = Directory.GetFiles(appDirectory + "dependency");
             string procargs=Environment.ExpandEnvironmentVariables(
-                    "%JAVA_OPTS% " +
                     "-XX:+UseSerialGC " +
                     "-Dderby.system.home=%USERPROFILE% " +
                     "-Dfile.encoding=UTF-8 " +
