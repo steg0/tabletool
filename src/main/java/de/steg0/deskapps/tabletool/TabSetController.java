steg0/tabletool

commit 019dc9063a9846df265779a8f02985c766aea0fe

close before opening workspace (for retries)
git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@7603 7869264c-0633-0410-bb99-d298f671b333

src/main/java/de/steg0/deskapps/tabletool/TabSetController.java
@@ -802,6 +802,8 @@ implements KeyListener
         {
             Workspace w = Workspaces.load(workspaceFile);
             
+            closeWorkspace0(false);
+
             if(w.getRecentFiles() != null)
             {
                 for(var path : w.getRecentFiles()) recents.add(path);
@@ -898,6 +900,12 @@ implements KeyListener
                 return false;
             }
         }
+        closeWorkspace0(addUntitled);
+        return true;
+    }
+
+    private void closeWorkspace0(boolean addUntitled)
+    {
         while(!notebooks.isEmpty())
         {
             NotebookController notebook = notebooks.get(0);
@@ -908,7 +916,6 @@ implements KeyListener
         if(addUntitled) add(-1);
         workspaceFile = null;
         recents.clear();
-        return true;
     }
 
     void saveWorkspace()

commit a04f74904e31738e3e98403a5fdce5fc5d384282

fix bug introduced by r7603
closing workspace before retries also sets the file to null,
which is not good for the initial load


git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@7612 7869264c-0633-0410-bb99-d298f671b333

src/main/java/de/steg0/deskapps/tabletool/TabSetController.java
@@ -802,7 +802,7 @@ implements KeyListener
         {
             Workspace w = Workspaces.load(workspaceFile);
             
-            closeWorkspace0(false);
+            removeNotebooks();
 
             if(w.getRecentFiles() != null)
             {
@@ -900,11 +900,13 @@ implements KeyListener
                 return false;
             }
         }
-        closeWorkspace0(addUntitled);
+        removeNotebooks();
+        workspaceFile = null;
+        if(addUntitled) add(-1);
         return true;
     }
 
-    private void closeWorkspace0(boolean addUntitled)
+    private void removeNotebooks()
     {
         while(!notebooks.isEmpty())
         {
@@ -913,8 +915,6 @@ implements KeyListener
             notebooks.remove(0);
             tabbedPane.remove(0);
         }
-        if(addUntitled) add(-1);
-        workspaceFile = null;
         recents.clear();
     }
 