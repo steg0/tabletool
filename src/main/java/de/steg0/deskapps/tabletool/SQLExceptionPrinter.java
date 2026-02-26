939e2164f9867f93cd6416df32411f0a8bcf5c33
rs <rs@7869264c-0633-0410-bb99-d298f671b333>
2026-02-26 21:21:27 +0000

    hack in support for MSSQL line number info
    git-svn-id: https://hq.steg0.eu/svn/trunk/tabletool@7685 7869264c-0633-0410-bb99-d298f671b333

--- src/main/java/de/steg0/deskapps/tabletool/SQLExceptionPrinter.java
+++ src/main/java/de/steg0/deskapps/tabletool/SQLExceptionPrinter.java
@@ -1,10 +1,15 @@
 package de.steg0.deskapps.tabletool;
 
+import java.beans.Introspector;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class SQLExceptionPrinter
 {
+    static Logger logger = Logger.getLogger("tabtype");
+
     static String toString(SQLException e)
     {
         StringBuilder b=new StringBuilder();
@@ -12,6 +17,7 @@ class SQLExceptionPrinter
         {
             b.append("Error code: "+e.getErrorCode()+"\n");
             b.append("SQL State: "+e.getSQLState()+"\n");
+            b.append(describeVendorSpecificProperties(e));
             b.append(e.getMessage()+"\n");
             var cause = e.getCause();
             if(cause != null)
@@ -22,6 +28,37 @@ class SQLExceptionPrinter
         b.append("Driver reported error at ");
         b.append(new Date());
         return b.toString();
+    }
+
+    private static String describeVendorSpecificProperties(SQLException se)
+    {
+        try
+        {
+            var beanInfo = Introspector.getBeanInfo(se.getClass());
+            for(var pd : beanInfo.getPropertyDescriptors())
+            {
+                switch(pd.getName())
+                {
+                case "SQLServerError":
+                    var property = pd.getReadMethod().invoke(se);
+                    beanInfo = Introspector.getBeanInfo(property.getClass());
+                    for(var pd0 : beanInfo.getPropertyDescriptors())
+                    {
+                        switch(pd0.getName())
+                        {
+                            case "lineNumber":
+                                return "lineNumber="+pd0.getReadMethod()
+                                        .invoke(property)+"\n";
+                        }
+                    }
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            logger.log(Level.INFO,"Error during SQLException introspection",e);
+        }
+        return "";
     }
 
     static String toString(String sql,SQLException e)
